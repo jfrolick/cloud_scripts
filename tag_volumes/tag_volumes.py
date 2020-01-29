225e6acb5562833c1f4fddf4fcc3bb645aa6a2fa
sg0890929 <9579406+jfrolick@users.noreply.github.com>
2020-01-29 14:25:43 -0600

    Almost there.

--- tag_volumes/tag_volumes.py
+++ tag_volumes/tag_volumes.py
@@ -1,22 +1,84 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import json
 import boto3
 import sys
 import re
 import ipdb
+import argparse
+import os
 
+
+allowedtags = [
+    'Name',
+    'Owner',
+    'BusinessUnit',
+    'BusinessService',
+    'TechnicalService',
+    'Environment',
+    'ContactEmail',
+    'Hostname'
+]
+
+
+#
+# Parse arguments
+#
+
+parser = argparse.ArgumentParser(description='Tag instances.')
+
+parser.add_argument(
+    '--region',
+    '-r',
+    default='us-east-1',
+    help='AWS region name'
+)
+
+parser.add_argument(
+    '--debug',
+    '-d',
+    default=False,
+    help="Debug",
+    dest='debug',
+    action='store_true'
+)
+
+args = parser.parse_args()
+
+if args.region:
+    os.environ['DEFAULT_AWS_REGION'] = args.region
+
+print("AWS Region: " + os.environ['DEFAULT_AWS_REGION'])
+
+
+#
+# Connect to AWS
+#
 
 #ec2 = boto3.resource('ec2', region_name='us-east-1')
 
-client = boto3.client('ec2')
+ec2 = boto3.client('ec2')
 
-# Get instances
-reservations = client.describe_instances().get(
+print("Describing instances.")
+try:
+    reservations = ec2.describe_instances().get(
         'Reservations', []
-)
+        )
+except ClientError as e:
+    print("Unexpected error: %s" % e)
+    exit(1)
 
-#ipdb.set_trace()
+print("Done.")
+
+
+if args.debug == True:
+    print("Debugging console - loading ipdb.set_trace()")
+    ipdb.set_trace()
+
+
+#
+# Flatten tags function
+#
 
 def flatten_tags(tags):
 
@@ -30,9 +92,25 @@ def flatten_tags(tags):
 
     return flattened
 
-ipdb.set_trace()
 
-#for i in reservations:
-#    for j in i['Instances']:
+for r in reservations:
+    for i in r['Instances']:
         # ipdb.set_trace()
-        # print j['InstanceId']
+        tags = flatten_tags(i['Tags'])
+        newtags = []
+        if 'Name' not in tags:
+            print("Skipping Instance" + ' ' + i['InstanceId'])
+            continue
+        # print (i)
+        # print (i['Tags'])
+        for b in i['BlockDeviceMappings']:
+            print (i['InstanceId'] + ' ' + b['DeviceName'] + " " + b['Ebs']['VolumeId'] + " " + b['Ebs']['Status'])
+            for key, value in tags.items():
+                if key not in allowedtags:
+                    continue
+                if key == 'Name':
+                    device = re.sub("\/dev\/",'',b['DeviceName'])
+                    print ("Adding: " + key + ": " + value + "-" + device)
+                    newtags
+                else:
+                    print ("Adding: " + key + ": " + value)
