735145c6306820dfed166499a954d961b5e17ef7
sg0890929 <9579406+jfrolick@users.noreply.github.com>
2020-04-02 14:43:22 -0500

    Add logic to find groups that would have problem with transit gateway and change output format

--- secgroups/dump-secgroups.py
+++ secgroups/dump-secgroups.py
@@ -22,6 +22,19 @@ except ClientError as e:
     print(e)
 
 
+# Get the account ID
+
+def get_account_number():
+
+    _client = boto3.client("sts")
+
+    account_id = _client.get_caller_identity()["Account"]
+
+    return account_id
+
+
+account_id = get_account_number()
+
 # Loop through groups
 
 for group in secgroups['SecurityGroups']:
@@ -33,38 +46,67 @@ for group in secgroups['SecurityGroups']:
 
         if ippermission['IpProtocol'] == "tcp":
             protocol = ippermission['IpProtocol']
-            fromport = ippermission['FromPort']
-            toport = ippermission['ToPort']
+            fromport = str(ippermission['FromPort'])
+            toport = str(ippermission['ToPort'])
+        elif ippermission['IpProtocol'] == "udp":
+            protocol = ippermission['IpProtocol']
+            fromport = str(ippermission['FromPort'])
+            toport = str(ippermission['ToPort'])
         elif ippermission['IpProtocol'] == "-1":
             protocol = "all"
             fromport = "0"
             toport = "65535"
+        else:
+            protocol = "unknown"
+            fromport = "unknown"
+            toport = "unknown"
 
         for rule in ippermission['UserIdGroupPairs']:
             empty = False
             print(
-                "Ingress: ",
-                group['GroupId'],
-                group['GroupName'],
-                group['VpcId'],
-                protocol,
-                fromport,
-                toport,
-                rule['GroupId'],
-                rule['UserId']
+                "AccountId:" +  account_id + ", " +
+                "Ingress:(" +
+                "GroupId:" +  group['GroupId'] + ",",
+                "GroupName:\"" +  group['GroupName'] + "\",",
+                "GroupVpcId:" +  group['VpcId'] + ",",
+                "Protocol:" +  protocol + ",",
+                "Ports:" +  fromport + "-" +  toport + ",",
+                "IngressGroup:" +  rule['GroupId'] + ",",
+                "IngressAccount:" +  rule['UserId'] +
+                ")"
             )
+
+            # Find group VPC
+            groupvpc = list(
+                filter(
+                    lambda x: x['GroupId'] == rule['GroupId'],
+                    secgroups['SecurityGroups']
+                )
+            )
+
+            if len(groupvpc) > 0:
+                if group['VpcId'] != groupvpc[0]['VpcId']:
+                    print(
+                        "AccountId:" +  account_id + ", " +
+                        "Warning:\"(TGW) Security group VpcId does not match ingress rule security group VPC ID\",",
+                        "GroupId:" +  group['GroupId'] + ",",
+                        "GroupVpcId:" +  group['VpcId'] + ",",
+                        "IngressVpcId:" +  groupvpc[0]['VpcId']
+                    )
+            else:
+                print ("Ingress rule group ID does not exist - " +  rule['GroupId'])
 
         for rule in ippermission['IpRanges']:
             empty = False
             print(
-                "Ingress: ",
-                group['GroupId'],
-                group['GroupName'],
-                group['VpcId'],
-                protocol,
-                fromport,
-                toport,
-                rule['CidrIp'],
+                "AccountId:" +  account_id + ", " +
+                "Ingress:(" +
+                "GroupId:" +  group['GroupId'] + ",",
+                "GroupName:\"" +  group['GroupName'] + "\",",
+                "GroupVpcId:" +  group['VpcId'] + ",",
+                "Protocol:" +  protocol + ",",
+                "Ports:" +  fromport + "-" +  toport + ",",
+                "CIDR:" +  rule['CidrIp'] + ")"
             )
 
 
@@ -72,47 +114,77 @@ for group in secgroups['SecurityGroups']:
 
     for ippermission in group['IpPermissionsEgress']:
 
+
         if ippermission['IpProtocol'] == "tcp":
             protocol = ippermission['IpProtocol']
-            fromport = ippermission['FromPort']
-            toport = ippermission['ToPort']
+            fromport = str(ippermission['FromPort'])
+            toport = str(ippermission['ToPort'])
+        elif ippermission['IpProtocol'] == "udp":
+            protocol = ippermission['IpProtocol']
+            fromport = str(ippermission['FromPort'])
+            toport = str(ippermission['ToPort'])
         elif ippermission['IpProtocol'] == "-1":
             protocol = "all"
             fromport = "0"
             toport = "65535"
+        else:
+            protocol = "unknown"
+            fromport = "unknown"
+            toport = "unknown"
 
         for rule in ippermission['UserIdGroupPairs']:
             empty = False
             print(
-                "Egress: ",
-                group['GroupId'],
-                group['GroupName'],
-                group['VpcId'],
-                protocol,
-                fromport,
-                toport,
-                rule['GroupId'],
-                rule['UserId']
+                "AccountId:" +  account_id + ", " +
+                "Egress:(" +
+                "GroupId:" +  group['GroupId'] + ",",
+                "GroupName:\"" +  group['GroupName'] + "\",",
+                "GroupVpcId:" +  group['VpcId'] + ",",
+                "Protocol:" +  protocol + ",",
+                "Ports:" +  fromport + "-" +  toport + ",",
+                "EgressGroup:" +  rule['GroupId'] + ",",
+                "EgressAccount:" +  rule['UserId'] +
+                ")"
             )
+
+            # Find group VPC
+            groupvpc = list(
+                filter(
+                    lambda x: x['GroupId'] == rule['GroupId'],
+                    secgroups['SecurityGroups']
+                )
+            )
+
+            if len(groupvpc) > 0:
+                if group['VpcId'] != groupvpc[0]['VpcId']:
+                    print(
+                        "AccountId:" +  account_id + ", " +
+                        "Warning:\"(TGW) Security group VpcId does not match egress rule security group VPC ID\",",
+                        "GroupId:" +  group['GroupId'] + ",",
+                        "GroupVpcId:" +  group['VpcId'] + ",",
+                        "EgressVpcId:" +  groupvpc[0]['VpcId']
+                    )
+            else:
+                print ("Egress rule group ID does not exist - " +  rule['GroupId'])
 
         for rule in ippermission['IpRanges']:
             empty = False
             print(
-                "Egress: ",
-                group['GroupId'],
-                group['GroupName'],
-                protocol,
-                fromport,
-                toport,
-                group['VpcId'],
-                rule['CidrIp'],
+                "AccountId:" +  account_id + ", " +
+                "Egress:(" +
+                "GroupId:" +  group['GroupId'] + ",",
+                "GroupName:\"" +  group['GroupName'] + "\",",
+                "GroupVpcId:" +  group['VpcId'] + ",",
+                "Protocol:" +  protocol + ",",
+                "Ports:" +  fromport + "-" +  toport + ",",
+                "CIDR:" +  rule['CidrIp'] + ")"
             )
 
     # Empty groups - No ingress or egress rules
     if empty:
         print(
-            "Group: ",
-            group['GroupId'],
-            group['GroupName'],
-            group['VpcId'],
+            "AccountId:" +  account_id + ", " +
+            "GroupId:" +  group['GroupId'],
+            "GroupName:\"" +  group['GroupName'] + "\"",
+            "GroupVpcId:" +  group['VpcId'],
         )
